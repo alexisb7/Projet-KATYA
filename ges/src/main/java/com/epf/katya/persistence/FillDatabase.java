fa40edbc16fbabdcb920f1cd1a5b86175644677c
Alexis Bériot <alexis.beriot@epfedu.fr>
2022-06-02 16:57:42 +0200

    init Database

--- ges/src/main/java/com/epf/katya/persistence/FillDatabase.java
+++ ges/src/main/java/com/epf/katya/persistence/FillDatabase.java
@@ -16,7 +16,7 @@ public class FillDatabase {
 
     public static void main(String[] args) throws Exception {
         try {
-            DeleteDbFiles.execute("~", "RentManagerDatabase", true);
+            DeleteDbFiles.execute("~", "KatyaDatabase", true);
             insertWithPreparedStatement();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -28,10 +28,7 @@ public class FillDatabase {
         PreparedStatement createPreparedStatement = null;
 
         List<String> createTablesQueries = new ArrayList<>();
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Client(id INT primary key auto_increment, client_id INT, nom VARCHAR(100), prenom VARCHAR(100), email VARCHAR(100), naissance DATETIME)");
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Vehicle(id INT primary key auto_increment, constructeur VARCHAR(100), nb_places TINYINT(255))");
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Reservation(id INT primary key auto_increment, client_id INT, foreign key(client_id) REFERENCES Client(id), vehicle_id INT, foreign key(vehicle_id) REFERENCES Vehicle(id), debut DATETIME, fin DATETIME)");
-
+        
         try {
             connection.setAutoCommit(false);
 
@@ -42,16 +39,7 @@ public class FillDatabase {
             }
 
             // Remplissage de la base avec des Vehicules et des Clients
-            Statement stmt = connection.createStatement();
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Renault', 4)");
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Peugeot', 4)");
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Seat', 4)");
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Nissan', 4)");
             
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Dupont', 'Jean', 'jean.dupont@email.com', '1988-01-22')");
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Morin', 'Sabrina', 'sabrina.morin@email.com', '1988-01-22')");
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Afleck', 'Steeve', 'steeve.afleck@email.com', '1988-01-22')");
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Rousseau', 'Jacques', 'jacques.rousseau@email.com', '1988-01-22')");
                     
             connection.commit();
             System.out.println("Success!");
