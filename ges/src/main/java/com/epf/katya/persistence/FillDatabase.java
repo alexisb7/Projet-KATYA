2c6b53a44b6447738c78ce7e3cdc5a408e1c1a87
Tristan Bourhis <tristan.bourhis@epfedu.fr>
2022-06-02 17:46:08 +0200

    BDD part 1

--- ges/src/main/java/com/epf/katya/persistence/FillDatabase.java
+++ ges/src/main/java/com/epf/katya/persistence/FillDatabase.java
@@ -28,10 +28,11 @@ public class FillDatabase {
         PreparedStatement createPreparedStatement = null;
 
         List<String> createTablesQueries = new ArrayList<>();
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Client(id INT primary key auto_increment, client_id INT, nom VARCHAR(100), prenom VARCHAR(100), email VARCHAR(100), naissance DATETIME)");
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Vehicle(id INT primary key auto_increment, constructeur VARCHAR(100), nb_places TINYINT(255))");
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Reservation(id INT primary key auto_increment, client_id INT, foreign key(client_id) REFERENCES Client(id), vehicle_id INT, foreign key(vehicle_id) REFERENCES Vehicle(id), debut DATETIME, fin DATETIME)");
-
+        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Equipement(id_equipement INT primary key, nom_equipement VARCHAR(25), zone_de_stockage VARCHAR(50), etat VARCHAR(50), disponibilite_equipement INT, date_aquisition DATETIME, description VARCHAR(50))");
+        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Utilisateur(id_utilisateur INT primary key, email VARCHAR(30), mdp VARCHAR(25), nom_utilisateur VARCHAR(25), role VARCHAR(25), date_entree DATETIME)");
+        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Salle(numero VARCHAR(10) primary key, etage INT, capacite INT, utilite VARCHAR(25), disponibilite_salle INT, date_acquisition_salle DATETIME)");
+        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Reservation_equipement(id_reservation_equipement INT primary key, id_utilisateur INT, foreign key(id_utilisateur) REFERENCES Utilisateur(id_utilisateur), date_debut DATETIME, date_fin DATETIME, id_utilisateur_validation INT, foreign key(id_utilisateur_validation) REFERENCES Utilisateur(id_utilisateur))");
+        
         try {
             connection.setAutoCommit(false);
 
@@ -41,18 +42,8 @@ public class FillDatabase {
 	            createPreparedStatement.close();
             }
 
-            // Remplissage de la base avec des Vehicules et des Clients
+            // Remplissage de la base
             Statement stmt = connection.createStatement();
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Renault', 4)");
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Peugeot', 4)");
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Seat', 4)");
-            stmt.execute("INSERT INTO Vehicle(constructeur, nb_places) VALUES('Nissan', 4)");
-            
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Dupont', 'Jean', 'jean.dupont@email.com', '1988-01-22')");
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Morin', 'Sabrina', 'sabrina.morin@email.com', '1988-01-22')");
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Afleck', 'Steeve', 'steeve.afleck@email.com', '1988-01-22')");
-            stmt.execute("INSERT INTO Client(nom, prenom, email, naissance) VALUES('Rousseau', 'Jacques', 'jacques.rousseau@email.com', '1988-01-22')");
-                    
             connection.commit();
             System.out.println("Success!");
         } catch (SQLException e) {
