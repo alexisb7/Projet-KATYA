3a6799d07c97ecfb5283d555ba6b8d28f5f064ae
Alexis Bériot <alexis.beriot@epfedu.fr>
2022-06-02 20:03:40 +0200

    initialize dao

--- ges/src/main/java/com/epf/katya/dao/ReservationSalleDao.java
+++ ges/src/main/java/com/epf/katya/dao/ReservationSalleDao.java
@@ -2,4 +2,14 @@ package com.epf.katya.dao;
 
 public class ReservationSalleDao {
     
+    private static ReservationSalleDao instance = null;
+    private ReservationSalleDao() {}
+
+    public static ReservationSalleDao getInstance() {
+        if(instance == null){
+            instance = new ReservationSalleDao();
+        }
+        return instance;
+    }
+
 }
