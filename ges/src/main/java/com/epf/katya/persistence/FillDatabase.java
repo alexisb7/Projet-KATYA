4ea6517e43546e129bcada4ea09858201ea96154
Tristan Bourhis <tristan.bourhis@epfedu.fr>
2022-06-02 18:57:46 +0200

    add tomcat

--- ges/src/main/java/com/epf/katya/persistence/FillDatabase.java
+++ ges/src/main/java/com/epf/katya/persistence/FillDatabase.java
@@ -33,7 +33,7 @@ public class FillDatabase {
         createTablesQueries.add("CREATE TABLE IF NOT EXISTS Salle(numero VARCHAR(10) primary key, etage INT, capacite INT, utilite VARCHAR(25), disponibilite_salle INT, date_acquisition_salle DATETIME)");
         createTablesQueries.add("CREATE TABLE IF NOT EXISTS Reservation_equipement(id_reservation_equipement INT primary key, id_equipement INT, foreign key(id_equipement) REFERENCES Equipement(id_equipement), id_utilisateur INT, foreign key(id_utilisateur) REFERENCES Utilisateur(id_utilisateur), etat_validation INT, date_debut DATETIME, date_fin DATETIME, id_utilisateur_validation INT, foreign key(id_utilisateur_validation) REFERENCES Utilisateur(id_utilisateur))");
         createTablesQueries.add("CREATE TABLE IF NOT EXISTS Reservation_salle(id_reservation_salle INT primary key, numero_salle INT, foreign key(numero_salle) REFERENCES Salle(numero), id_utilisateur INT, foreign key(id_utilisateur) REFERENCES Utilisateur(id_utilisateur), etat_validation INT, date_debut DATETIME, date_fin DATETIME, id_utilisateur_validation INT, foreign key(id_utilisateur_validation) REFERENCES Utilisateur(id_utilisateur))");
-        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Document(id_document INT primary key, lien VARCHAR(50))");
+        createTablesQueries.add("CREATE TABLE IF NOT EXISTS Document(id_document INT primary key, lien VARCHAR(250))");
 
         try {
             connection.setAutoCommit(false);
