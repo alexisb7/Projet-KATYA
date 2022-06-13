06b0dde5c39ae75b81f2bbe14e8761d61db69ccb
Alexis Bériot <alexis.beriot@epfedu.fr>
2022-06-13 13:01:10 +0200

    delete target

--- ges/src/main/java/com/epf/katya/ui/servlets/SalleServlet.java
+++ ges/src/main/java/com/epf/katya/ui/servlets/SalleServlet.java
@@ -6,20 +6,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
 import java.io.IOException;
 
-<<<<<<< HEAD
-@WebServlet("/salle")
-=======
-@WebServlet("/salles")
->>>>>>> 59357f7f2bca09e448d27eefaaf72f1600719efb
-public class SalleServlet extends HttpServlet {
-//    public void init() throws ServletException {
-//        super.init();
-//        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-//    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+@WebServlet("/equipement")
+public class SalleServlet extends HttpServlet {
+
+   public void init() throws ServletException {
+       super.init();
+       SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+   }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
 
         RequestDispatcher r = request.getRequestDispatcher("WEB-INF/views/salle.jsp");
